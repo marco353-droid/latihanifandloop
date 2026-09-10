e9bbd17b601a34107b4bdd5f2d0f1fbfdbe230ff
Marco Charlos <marcocharlos353@gmail.com>
2026-09-10 19:43:38 +0700

    Version 1

--- ifnloop.dart
+++ ifnloop.dart
@@ -1,9 +1,127 @@
+
 void main() {
 
-int angka = 0;
-if (angka % 2 == 0){
-  print("Angka $angka adalah bilangan genap");
-} else {
-  print("Angka $angka adalah bilangan ganjil");
-//hai
-}}
+//int angka = 5;
+//var nilai = 90;
+
+
+//if (angka % 2 == 0){
+ // print("Angka $angka adalah bilangan genap");
+//} else {
+//  print("Angka $angka adalah bilangan ganjil");
+//hai dunia hello world 
+//}
+
+
+
+
+
+// IF dieksekusi dua kali karena ada 2 kondisi yang menggunakan if 
+//if (nilai > 85) {
+ // print("Nilai $nilai adalah lebih besar dari 85");
+//}
+//if (nilai < 85) {
+//  print("Nilai $nilai adalah lebih kecil dari 85");
+//}
+
+// switch case digunakan untuk mengecek nilai yang sama dengan case yang ada
+// switch digunakan  jika nilainya pasti
+//var nilai = '';
+ 
+
+
+//print("Mohon Masukkan nilai (A, B, C, D): ");
+//nilai = stdin.readLineSync()!;
+
+ //switch (nilai) {
+  //case 'A':
+    //print("Nilai $nilai adalah sangat baik");
+    //break;
+  //case 'B':
+    //print("Nilai $nilai adalah baik");
+   // break;
+  //case 'C':
+    //print("Nilai $nilai adalah cukup");
+    //break;
+  //case 'D':
+    //print("Nilai $nilai adalah kurang");
+    //break;
+  //default:
+    //print("Nilai $nilai tidak valid");
+
+
+// for
+// for in
+// foreach
+// while
+//do while
+
+
+//for (var i = 0; i <= 10; i++) {
+  //print("Perulangan ke-$i");
+
+
+//List datamhs = ['Deni', 'Windy', 'Sindy', 'Jonathan', 'Michael'];
+//for (var i = 0; i < datamhs.length; i++) {
+  //print("Nama Mahasiswa ke-$i adalah ${datamhs[i]}");
+
+//if (i == 0) {
+  //  print("Nama Mahasiswa ke-$i adalah ${datamhs[i]}");
+ // }
+
+
+
+// for in
+
+//List datamhs = ['Deni', 'Windy', 'Sindy', 'Jonathan', 'Michael'];
+//for (var nama in datamhs) {
+  //print("Nama Mahasiswa adalah $nama");
+  //if (nama == 'Sindy') {
+    //print("Nama Mahasiswa adalah $nama");
+  //}
+//} 
+
+
+
+// for each (khusus untuk list)
+//List datamhs = ['Deni', 'Windy', 'Sindy', 'Jonathan', 'Michael'];
+//datamhs.forEach((a) {
+// print(a);
+//  if (a == 'Sindy') {
+//    print(a);
+//  }
+//});
+
+
+// while loop
+
+//var nilai = 0;
+//while (nilai < 5) {
+//  print(nilai);
+//nilai++;
+//}
+
+
+
+//List datamhs = ['Deni', 'Windy', 'Sindy', 'Jonathan', 'Michael'];
+//var index = 0;
+//while (index < datamhs.length) {
+//  print(datamhs[index]);
+//  index++;
+//}
+
+
+
+// do while loop
+// print dulu setelah itu membaca kondisi, jadi akan dieksekusi minimal 1 kali
+
+//List datamhs = ['Deni', 'Windy', 'Sindy', 'Jonathan', 'Michael'];
+//var index = 0;
+//do {
+//  print(datamhs[index]);
+//  index++;
+//} while (index < datamhs.length);
+
+
+
+}
